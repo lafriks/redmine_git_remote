c876a88ca05f33ad57f69ce52d24a106bb48eb05
COLA@Redminetest <carloskar.larsson@gmail.com>
2017-08-05 16:03:03 +0200

    Fixed missing default prefix.

--- lib/redmine_git_remote/repositories_helper_patch.rb
+++ lib/redmine_git_remote/repositories_helper_patch.rb
@@ -12,8 +12,16 @@ module RedmineGitRemote
                                          :label => l(:field_path_to_repository))
         local_path_note = content_tag('em', l(:text_git_remote_path_note), :class => 'info')
 
+        if repository.id.nil?
+          remote_url_value = Setting.plugin_redmine_git_remote["git_remote_url_prefix_default"].presence || ''
+        else
+          remote_url_value=repository.extra_clone_url
+        end
+
         remote_url_tag = form.text_field(:extra_clone_url, :size => 60, :required => true,
+                                         :value => remote_url_value,
                                          :disabled => !repository.safe_attribute?('url'))
+
         remote_url_note = content_tag('em', l(:text_git_remote_url_note), :class => 'info')
 
         git_remote_tag = content_tag('p',  local_path_tag + local_path_note + remote_url_tag + remote_url_note)
